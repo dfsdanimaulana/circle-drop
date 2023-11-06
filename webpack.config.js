7b1b1be1cfe9bb3ee69bd2764abbc4dfdbd92276
dfsdanimaulana <danimaulana9f@gmail.com>
2023-11-06 18:15:06 +0700

    add pwa and remove unused webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const WorkboxPlugin = require("workbox-webpack-plugin")
 
 const isProduction = process.env.NODE_ENV == "production"
-
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : "style-loader"
 
 const config = {
     entry: "./src/index.js",
@@ -15,6 +13,12 @@ const config = {
     plugins: [
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
     ],
     module: {
         rules: [
@@ -22,15 +26,6 @@ const config = {
                 test: /\.(js|jsx)$/i,
                 loader: "babel-loader",
             },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler, "css-loader"],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: "asset",
-            },
-
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
         ],
@@ -40,8 +35,6 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = "production"
-
-        config.plugins.push(new MiniCssExtractPlugin())
     } else {
         config.mode = "development"
     }
