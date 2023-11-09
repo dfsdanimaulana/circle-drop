fe186122724ac8a4f880cbb25604e2b2fccadcd9
dfsdanimaulana <danimaulana9f@gmail.com>
2023-11-09 12:50:34 +0700

    put GenerateSW in if condition

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,6 @@ const config = {
     plugins: [
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-        new WorkboxPlugin.GenerateSW({
-            // these options encourage the ServiceWorkers to get in there fast
-            // and not allow any straggling "old" SWs to hang around
-            clientsClaim: true,
-            skipWaiting: true,
-        }),
     ],
     module: {
         rules: [
@@ -35,6 +29,14 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = "production"
+        config.plugins.push(
+            new WorkboxPlugin.GenerateSW({
+                // these options encourage the ServiceWorkers to get in there fast
+                // and not allow any straggling "old" SWs to hang around
+                clientsClaim: true,
+                skipWaiting: true,
+            }),
+        )
     } else {
         config.mode = "development"
     }
